e60c25734fe613a746e941305bf590d8ce58006f
peter-miles <103599491@student.swin.edu.au>
2021-04-20 18:44:52 +1000

    added array tasks 1 & 2, and list task 1

---
+++ program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arrays_and_lists {
+    class program {
+        static void Main(string[] args) {
+            
+
+            /*array_example_1();
+            Console.WriteLine(array_example_2());*/
+        }
+
+        static public void array_example_1() {
+            int array_size = 5;
+
+            string[] names = new string[array_size];
+             
+            names[0] = "Steven"; 
+            names[1] = "Bob"; 
+            names[2] = "Will";
+            names[3] = "James"; 
+            names[4] = "Matt";
+            
+            for (int i = 0; i < array_size; i ++) {
+                Console.WriteLine("Hello " + names[i]);
+            }
+        }
+
+        static public int array_example_2() {
+            int[] numbers = { 34,5,67,1,99,34,44,78,34,0 };
+
+            int ret = 0;
+
+            foreach (int i in numbers) {
+                ret += i;
+            }
+
+            return ret;
+        }
+
+        static public void list_example_1(int input) {
+            var list = new List<int>(); 
+            var random = new Random();
+
+            for (int i = 0; i < 5; i ++) {
+                list.Add(random.Next(1, 10));
+                Console.WriteLine(input == list[i] ? list[i] + " <" : list[i]);
+            }
+        }
+    }
+}
